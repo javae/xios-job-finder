03cc99f949170eca681c7616c972259eb8156f07
tom.verdeyen@gmail.com <tom.verdeyen@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-02-09 13:09:16 +0000

    Created settings activity

---
+++ JobFinder/src/xios/be/jobfinder/SettingsFragment.java
@@ -0,0 +1,15 @@
+package xios.be.jobfinder;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.preferences);
+	}
+	
+}
---
+++ JobFinder/src/xios/be/jobfinder/TimePickerFragment.java
@@ -0,0 +1,9 @@
+package xios.be.jobfinder;
+
+import android.support.v4.app.DialogFragment;
+
+public class TimePickerFragment extends DialogFragment {
+	
+	
+	
+}
