7a0c8e56ee61a27478bccb8188cdd27580c592f9
tom.verdeyen@gmail.com <tom.verdeyen@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-02-20 22:46:56 +0000

    Created settings screen with preferences API

--- JobFinder/src/be/xios/jobfinder/SettingsFragment.java
+++ JobFinder/src/be/xios/jobfinder/SettingsFragment.java
@@ -2,15 +2,33 @@ package be.xios.jobfinder;
 
 import be.xios.jobfinder.R;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 
 public class SettingsFragment extends PreferenceFragment {
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		addPreferencesFromResource(R.xml.preferences);
+
+		ListPreference savedSearchPreference = (ListPreference) findPreference("saved_search");
+		if (savedSearchPreference != null) {
+			savedSearchPreference.setSummary(savedSearchPreference.getValue());
+			savedSearchPreference.setOnPreferenceChangeListener(
+					new OnPreferenceChangeListener() {
+						@Override
+						public boolean onPreferenceChange(Preference preference, Object newValue) {
+							ListPreference configIdPreference = (ListPreference) preference;
+							configIdPreference.setSummary((String) newValue);
+							return true;
+						}
+					});
+		}
+
 	}
-	
+
 }
