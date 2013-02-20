31f4354284056381a5adff541a6748e6d93c302e
tom.verdeyen@gmail.com <tom.verdeyen@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-02-20 22:46:56 +0000

    Created settings screen with preferences API

--- trunk/JobFinder/src/be/xios/jobfinder/TimePickerFragment.java
+++ trunk/JobFinder/src/be/xios/jobfinder/TimePickerFragment.java
@@ -1,9 +1,30 @@
 package be.xios.jobfinder;
 
-import android.support.v4.app.DialogFragment;
+import java.util.Calendar;
 
-public class TimePickerFragment extends DialogFragment {
-	
-	
-	
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+public class TimePickerFragment extends DialogFragment implements
+		OnTimeSetListener {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		final Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+
+		return new TimePickerDialog(getActivity(), this, hour, minute,
+				DateFormat.is24HourFormat(getActivity()));
+	}
+
+	@Override
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+	}
 }
---
+++ trunk/JobFinder/src/be/xios/jobfinder/preference/TimePreference.java
@@ -0,0 +1,77 @@
+package be.xios.jobfinder.preference;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import be.xios.jobfinder.R;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.DialogPreference;
+import android.text.format.DateFormat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+public class TimePreference extends DialogPreference {
+
+	private TimePicker timePicker;
+
+	public TimePreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	@Override
+	protected View onCreateView(ViewGroup parent) {
+		SharedPreferences preferences = getSharedPreferences();
+		int hour = preferences.getInt(getKey() + ".hour", 8);
+		int minute = preferences.getInt(getKey() + ".minute", 30);
+		
+		try {
+			setSummary(getFormattedTime(hour, minute));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return super.onCreateView(parent);
+	}
+	
+	@Override
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		timePicker = (TimePicker) view.findViewById(R.id.pref_time_picker);
+		timePicker.setCurrentHour(getSharedPreferences().getInt(getKey() + ".hour", 8));
+		timePicker.setCurrentMinute(getSharedPreferences().getInt(getKey() + ".minute", 30));
+		timePicker.setIs24HourView(DateFormat.is24HourFormat(timePicker.getContext()));
+	}
+
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		super.onDialogClosed(positiveResult);
+
+		if (positiveResult) {
+			timePicker.clearFocus();
+			
+			int hour = timePicker.getCurrentHour();
+			int minute = timePicker.getCurrentMinute();
+			
+			try {
+				setSummary(getFormattedTime(hour, minute));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			SharedPreferences.Editor editor = getEditor();
+			editor.putInt(getKey() + ".hour", hour);
+			editor.putInt(getKey() + ".minute", minute);
+			editor.commit();
+		}
+	}
+	
+	private String getFormattedTime(int hour, int minute) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm", Locale.getDefault());
+		return DateFormat.getTimeFormat(getContext()).format(format.parse(hour + ":" + minute));
+	}
+}
