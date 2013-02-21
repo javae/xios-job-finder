9cddc1292b461dab051c907ca7e014976e5e85f9
tom.verdeyen@gmail.com <tom.verdeyen@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-02-21 17:51:59 +0000

    Doorlinken van SearchBuilderActivity naar JobSearchActivity en aanpassen strings

--- JobFinder/src/be/xios/jobfinder/SearchBuilderActivity.java
+++ JobFinder/src/be/xios/jobfinder/SearchBuilderActivity.java
@@ -3,18 +3,27 @@ package be.xios.jobfinder;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class SearchBuilderActivity extends Activity {
+
+	private Button search;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_params);
 		addItemsToCountrySpinner();
+
+		search = (Button) findViewById(R.id.btnSearch);
+		search.setOnClickListener(new SearchHandler());
 	}
 
 	@Override
@@ -25,12 +34,19 @@ public class SearchBuilderActivity extends Activity {
 	}
 
 	private void addItemsToCountrySpinner() {
-		 
 		Spinner spCountry = (Spinner) findViewById(R.id.spCountry);
 		List<Country> list = CountryData.getCountryList();
-		ArrayAdapter<Country> dataAdapter = new ArrayAdapter<Country>(this,
-			android.R.layout.simple_spinner_item, list);
+		ArrayAdapter<Country> dataAdapter = new ArrayAdapter<Country>(this,	android.R.layout.simple_spinner_item, list);
 		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spCountry.setAdapter(dataAdapter);
-	  }
+	}
+	
+	private class SearchHandler implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			Intent search = new Intent(getApplicationContext(), JobSearchActivity.class);
+			startActivity(search);
+		}
+    }
 }
