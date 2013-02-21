3aadd2b62d71b84989d45029f1e2c9abf100887c
janvaes@gmail.com <janvaes@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-02-21 15:31:58 +0000

    new icon getPostalCode & addMarkersToMap added

--- JobFinder/src/be/xios/jobfinder/MapActivity.java
+++ JobFinder/src/be/xios/jobfinder/MapActivity.java
@@ -1,6 +1,11 @@
 package be.xios.jobfinder;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import be.xios.jobfinder.R;
+import be.xios.jobfinder.model.LinkedInJob;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -8,7 +13,9 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,12 +30,12 @@ public class MapActivity extends Activity implements LocationListener {
 	private LocationManager locationManager;
 	private String provider;
 	private GoogleMap map;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		
+
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
 
@@ -67,45 +74,76 @@ public class MapActivity extends Activity implements LocationListener {
 		LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
 		map.addMarker(new MarkerOptions().position(ll).title("You're here."));
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 15));
-
-		// Geocoder gc = new Geocoder(getApplicationContext(),
-		// Locale.getDefault());
-		//
-		// try {
-		// List<Address> addresses = gc.getFromLocationName(
-		// "Lazarijstraat 141 Hasselt", 1);
-		//
-		// if (addresses.size() > 0) {
-		// LatLng position = new LatLng(addresses.get(0).getLatitude(),
-		// addresses.get(0).getLongitude());
-		// map.addMarker(new MarkerOptions().position(position).title("Home"));
-		// map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 12));
-		// // mapView.invalidate();
-		//
-		// }
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public String getPostalCodeFromCurrentLocation() {
+		String postalCode = "Not found";
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_LOW);
+		criteria.setAltitudeRequired(false);
+		criteria.setCostAllowed(false);
+		criteria.setSpeedRequired(false);
+		criteria.setBearingRequired(false);
+		criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+		provider = locationManager.getBestProvider(criteria, true);
+		Location location = locationManager.getLastKnownLocation(provider);
+
+		Geocoder gc = new Geocoder(getApplicationContext(), Locale.getDefault());
+		try {
+			List<Address> addresses = gc.getFromLocation(
+					location.getLatitude(), location.getLongitude(), 1);
+
+			if (addresses.size() > 0) {
+				postalCode = addresses.get(0).getPostalCode();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return postalCode;
+
+	}
+
+	public void addMarkersToMap(List<LinkedInJob> jobs) {
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		Geocoder gc = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+		for (LinkedInJob job : jobs) {
+			try {
+				List<Address> addresses = gc.getFromLocationName(job.getLocation(), 1);
+
+				if (addresses.size() > 0) {
+					LatLng ll = new LatLng(addresses.get(0).getLatitude(),
+							addresses.get(0).getLongitude());
+					map.addMarker(new MarkerOptions().position(ll).title(job.getPositionTitle()));
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
