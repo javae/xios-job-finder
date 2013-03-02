8057cf6274af661abf08a1d022d65134ca11a642
tom.verdeyen@gmail.com <tom.verdeyen@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-03-02 09:16:43 +0000

    Added method that accepts url

--- trunk/JobFinder/src/be/xios/jobfinder/connector/LinkedInConnector.java
+++ trunk/JobFinder/src/be/xios/jobfinder/connector/LinkedInConnector.java
@@ -11,13 +11,16 @@ import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 
+import be.xios.jobfinder.model.SearchBuilder;
+import be.xios.jobfinder.util.StringUtil;
+
 public class LinkedInConnector {
 
 	private static final String LINKED_IN_API_KEY = "98vfgp9ikj4k";
 	private static final String LINKED_IN_API_SECRET = "CxMoptwPqRISkG5f";
 	private static final String LINKED_IN_API_USER_TOKEN = "222277af-c1ea-4e3d-aa5e-b6b9ed4dc77b";
 	private static final String LINKED_IN_API_USER_SECRET = "64bc93d2-09c8-4fbf-af6f-7a15fff71e4c";
-	private static final String LINKED_IN_AUTH_URL = "https://www.linkedin.com/uas/oauth/authenticate";
+//	private static final String LINKED_IN_AUTH_URL = "https://www.linkedin.com/uas/oauth/authenticate";
 
 	private ServiceBuilder serviceBuilder;
 	private OAuthService authenticationService;
@@ -47,11 +50,38 @@ public class LinkedInConnector {
 	 *            The {@link JSONObject} to be added to the request.
 	 * @return The {@link InputStream} from which JSON data can be parsed.
 	 */
-	public InputStream sendRequest(Verb verb, String requestUrl, JSONObject requestObject) {
+	public InputStream sendRequest(Verb verb, String requestUrl, SearchBuilder searchBuilder) {
 		OAuthRequest request = new OAuthRequest(verb, requestUrl);
 		request.addHeader("x-li-format", "json");
 		request.addHeader("content-type", "application/json");
-		request.addPayload(requestObject.toString());
+//		request.addPayload(requestObject.toString());
+		
+		if (StringUtil.isNotBlank(searchBuilder.getKeywords()))
+			request.addQuerystringParameter("keywords", searchBuilder.getKeywords());
+		if (StringUtil.isNotBlank(searchBuilder.getJobTitle()))
+			request.addQuerystringParameter("job-title", searchBuilder.getJobTitle());
+		if (StringUtil.isNotBlank(searchBuilder.getCountryCode()))
+			request.addQuerystringParameter("country-code", searchBuilder.getCountryCode());
+		if (StringUtil.isNotBlank(searchBuilder.getPostalCode()))
+			request.addQuerystringParameter("postal-code", searchBuilder.getPostalCode());
+		if (searchBuilder.getDistance() > 0)
+			request.addQuerystringParameter("distance", "" + searchBuilder.getDistance());
+		
+		String facetsValue = "";
+		if (StringUtil.isNotBlank(searchBuilder.getIndustry()))
+			facetsValue += "industry";
+		if (StringUtil.isNotBlank(searchBuilder.getJobFunction())) {
+			facetsValue += !facetsValue.isEmpty() ? "," : "";
+			facetsValue += "job-function";
+		}
+
+		if (StringUtil.isNotBlank(facetsValue))
+			request.addQuerystringParameter("facets", facetsValue);
+		
+		if (StringUtil.isNotBlank(searchBuilder.getIndustry()))
+			request.addQuerystringParameter("facet", "industry," + searchBuilder.getIndustry());
+		if (StringUtil.isNotBlank(searchBuilder.getJobFunction()))
+			request.addQuerystringParameter("facet", "job-function," + searchBuilder.getJobFunction());
 
 		authenticationService.signRequest(accessToken, request);
 
@@ -59,4 +89,17 @@ public class LinkedInConnector {
 //		System.out.println(response.getBody());
 		return response.getStream();
 	}
+	
+	
+	public InputStream sendRequest(Verb verb, String requestUrl) {
+		OAuthRequest request = new OAuthRequest(verb, requestUrl);
+		request.addHeader("x-li-format", "json");
+		request.addHeader("content-type", "application/json");
+		authenticationService.signRequest(accessToken, request);
+		
+		Response response = request.send();
+//		System.out.println(response.getBody());
+		return response.getStream();
+	}
+	
 }
