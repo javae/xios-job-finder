813e901dbeaa104882b27d2c54433817d3783c99
janvaes@gmail.com <janvaes@gmail.com@0a4e01c1-f6b7-2631-394a-aab73dafda4e>
2013-03-08 14:23:31 +0000

    refactor favorites & savedsearch fragment to open selected item through click or contextmenu added icon for action toolbar

--- trunk/JobFinder/src/be/xios/jobfinder/main/SavedSearchFragment.java
+++ trunk/JobFinder/src/be/xios/jobfinder/main/SavedSearchFragment.java
@@ -19,35 +19,35 @@ import android.widget.ListView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class SavedSearchFragment extends ListFragment {
-	
+
 	/**
 	 * The fragment argument representing the item ID that this fragment
 	 * represents.
 	 */
 	public static final String ARG_ITEM_ID = "saved_search_fragment";
-	
+
 	private static final int myContextmenu_id = 1;
 	private static final int openMenuItem_Id = 2;
 	private static final int deleteMenuItem_Id = 3;
 	private JobFinderDAO datasource;
 	private ArrayAdapter<SearchBuilder> adapter;
-	
+
 	public SavedSearchFragment() {
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		datasource = new JobFinderDAO(getActivity().getApplicationContext());
 		datasource.open();
 
 		List<SearchBuilder> values = datasource.getAllSavedSearches();
-		adapter = new ArrayAdapter<SearchBuilder>(
-				getActivity(), android.R.layout.simple_list_item_1, values);
+		adapter = new ArrayAdapter<SearchBuilder>(getActivity(),
+				android.R.layout.simple_list_item_1, values);
 		setListAdapter(adapter);
 	}
-	
+
 	@Override
 	public void onResume() {
 		datasource.open();
@@ -65,32 +65,38 @@ public class SavedSearchFragment extends ListFragment {
 		super.onListItemClick(l, v, position, id);
 
 		SearchBuilder sb = (SearchBuilder) l.getAdapter().getItem(position);
+		openSavedSearch(sb);
+
+	}
+
+	private void openSavedSearch(SearchBuilder sb) {
 		Bundle arguments = new Bundle();
 		arguments.putParcelable(SearchResultFragment.SEARCH_PARAMS, sb);
-		
+
 		if (getActivity().findViewById(R.id.menuitem_detail_container) != null) {
 			SearchResultFragment fragment = new SearchResultFragment();
 			fragment.setArguments(arguments);
-			
-			FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+			FragmentTransaction transaction = getFragmentManager()
+					.beginTransaction();
 			transaction.replace(R.id.menuitem_detail_container, fragment);
 			transaction.addToBackStack(null);
 			transaction.commit();
 		} else {
-			Intent searchInt = new Intent(getActivity().getApplicationContext(), SearchResultActivity.class);
+			Intent searchInt = new Intent(
+					getActivity().getApplicationContext(),
+					SearchResultActivity.class);
 			searchInt.putExtras(arguments);
 			startActivity(searchInt);
 		}
-		
-		
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		registerForContextMenu(getListView());
 		super.onActivityCreated(savedInstanceState);
 	}
-	
+
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
@@ -109,31 +115,23 @@ public class SavedSearchFragment extends ListFragment {
 
 		switch (item.getItemId()) {
 		case openMenuItem_Id:
-			Intent searchInt = new Intent(getActivity(),
-					SearchResultActivity.class);
-
 			selectedSearch = (SearchBuilder) getListView().getAdapter()
 					.getItem(info.position);
-
-			Bundle b = new Bundle();
-			b.putParcelable(SearchResultFragment.SEARCH_PARAMS, selectedSearch);
-			searchInt.putExtras(b);
-			startActivity(searchInt);
+			openSavedSearch(selectedSearch);
 			break;
-			
+
 		case deleteMenuItem_Id:
 			selectedSearch = (SearchBuilder) getListView().getAdapter()
 					.getItem(info.position);
 			datasource.deleteSavedSearch(selectedSearch);
 			adapter.notifyDataSetChanged();
-			//TODO refresh werkt nog niet?
+			// TODO refresh werkt nog niet?
 			break;
-			
+
 		default:
 			break;
 		}
 		return super.onContextItemSelected(item);
 	}
 
-	
 }
